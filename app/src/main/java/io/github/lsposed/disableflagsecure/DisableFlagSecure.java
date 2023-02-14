ca71f12b84e0b14c85db980b41deb6639509758d
LoveSy <shana@zju.edu.cn>
2023-02-14 11:19:17 +0800

    Fix Android 13 support
    Fix #5

--- app/src/main/java/io/github/lsposed/disableflagsecure/DisableFlagSecure.java
+++ app/src/main/java/io/github/lsposed/disableflagsecure/DisableFlagSecure.java
@@ -1,6 +1,12 @@
 package io.github.lsposed.disableflagsecure;
 
 import android.os.Build;
+import android.util.Log;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.function.BiPredicate;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -9,6 +15,26 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class DisableFlagSecure implements IXposedHookLoadPackage {
+    private final static Method deoptimizeMethod;
+
+    static {
+        Method m = null;
+        try {
+            m = XposedBridge.class.getDeclaredMethod("deoptimizeMethod", Member.class);
+        } catch (Throwable t) {
+            XposedBridge.log(t);
+        }
+        deoptimizeMethod = m;
+    }
+
+    static void deoptimizeMethod(Class<?> c, String n) throws InvocationTargetException, IllegalAccessException {
+        for (Method m : c.getDeclaredMethods()) {
+            if (deoptimizeMethod != null && m.getName().equals(n)) {
+                deoptimizeMethod.invoke(null, m);
+                Log.d("DisableFlagSecure", "Deoptimized " + m);
+            }
+        }
+    }
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
@@ -27,6 +53,27 @@ public class DisableFlagSecure implements IXposedHookLoadPackage {
                             "isSecureLocked",
                             windowsState,
                             XC_MethodReplacement.returnConstant(false));
+                }
+            } catch (Throwable t) {
+                XposedBridge.log(t);
+            }
+            try {
+                deoptimizeMethod(XposedHelpers.findClass("com.android.server.wm.WindowStateAnimator", loadPackageParam.classLoader), "createSurfaceLocked");
+                var c = XposedHelpers.findClass("com.android.server.display.DisplayManagerService", loadPackageParam.classLoader);
+                deoptimizeMethod(c, "setUserPreferredModeForDisplayLocked");
+                deoptimizeMethod(c, "setUserPreferredDisplayModeInternal");
+                c = XposedHelpers.findClass("com.android.server.wm.InsetsPolicy$InsetsPolicyAnimationControlListener", loadPackageParam.classLoader);
+                for (var m : c.getDeclaredConstructors()) {
+                    deoptimizeMethod.invoke(null, m);
+                }
+                c = XposedHelpers.findClass("com.android.server.wm.InsetsPolicy", loadPackageParam.classLoader);
+                deoptimizeMethod(c, "startAnimation");
+                deoptimizeMethod(c, "controlAnimationUnchecked");
+                for (int i = 0; i < 20; i++) {
+                    c = XposedHelpers.findClassIfExists("com.android.server.wm.DisplayContent$$ExternalSyntheticLambda" + i, loadPackageParam.classLoader);
+                    if (c != null && BiPredicate.class.isAssignableFrom(c)) {
+                        deoptimizeMethod(c, "test");
+                    }
                 }
             } catch (Throwable t) {
                 XposedBridge.log(t);
